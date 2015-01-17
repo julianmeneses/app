7d07b7e8a35c87aa658072820fa7a4c39c52642e
julianmeneses <julian.meneses@gmail.com>
2015-01-16 21:15:53 -0500

    primera prueba

---
+++ programa.rb
@@ -0,0 +1,26 @@
+
+
+# Definición de variables
+julio=1
+lilia=2
+julian=3
+sandra=4
+sebastian=5
+
+
+# proceso principal
+if julio==lilia
+	puts "papa y mama iguales"
+else
+	puts "papa y mama diferentes"
+end
+
+
+# proceso secundario
+if "papa y mama iguales"
+	puts "proceso terminado"
+else
+	puts "proceso continua"
+end
+
+# 
